54823b22f6c730d3a8f4ff615dfd28b82f3e2c2c
VallesG <85541820+VallesG@users.noreply.github.com>
2021-12-10 21:35:24 -0500

    Add files via upload

---
+++ kali-update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Gil Valles 12-10-2021
+#me@gilvalles.com
+#This is a scipt to update Debian based Linux distrubtions, specifically  Kali Linux.
+#Must use sudo when running script.
+
+clear
+
+#creation of logfile and date/time variables
+logfile=/var/log/kali-update.log
+dt=$(date '+%d/%m/%Y  %H:%M:%S')
+
+#printing date/time and the user that ran the script to the logfile
+#printing empty lines for log file readability 
+echo "" >> $logfile
+echo -e "$dt $USER Kali Update" >> $logfile
+echo "Updating Kali Linux.  Please Wait"
+echo "" >> $logfile
+
+#fix any broken install(s) before updating
+apt --fix-broken install -y | tee -a $logfile
+
+#save the results of what needs to be upraged in a temporary text file
+apt list --upgradeable >> temp.txt
+
+#download package information | save output to logfile and print in terminal
+apt-get update -y | tee -a $logfile
+
+#upgrade all installed packages | save output to logfile and print in terminal
+apt-get upgrade -y | tee -a $logfile
+
+# remove and clean packages that are no longer needed | save output to logfile and print in terminal
+apt autoremove -y && apt autoclean -y | tee -a $logile
+
